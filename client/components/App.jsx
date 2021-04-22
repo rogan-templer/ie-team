a692703a87a4c7822a83511e9b75a730e2170093
Rogan-Templer <rmtempler@gmail.com>
2021-04-22 20:41:47 +1200

    Quiz and Question.jsx set up - button connects to quiz route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import Quiz from './Quiz'
 
 function App() {
   return (
     <>
       <Route path='/' component={Header} />
       <Route exact path='/' component={Home} />
+      <Route path='/quiz' component={Quiz} />
       <Route path='/' component={Footer} />
     </>
   )
---
+++ client/components/Quiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Question from './Question'
+
+function Quiz() {
+    return (
+        <div>
+            <p> <Question /> </p>
+        </div>
+    )
+}
+
+export default Quiz
