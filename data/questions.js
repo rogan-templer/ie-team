4048adef3d36ae42e33960c6728025625c42340a
Lorena Costa <loraphaela@gmail.com>
2021-04-22 17:36:55 +1200

    initial set up

---
+++ data/questions.js
@@ -0,0 +1,18 @@
+export default {
+  Questions: [
+    {
+      Question: "First question",
+      a: "Option",
+      b: "Option",
+      c: "Option",
+      d: "Option",
+    },
+    {
+      Question: "Second question",
+      a: "Option",
+      b: "Option",
+      c: "Option",
+      d: "Option",
+    },
+  ],
+};
