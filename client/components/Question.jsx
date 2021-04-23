2c9effbe6bdf3d91d1fe106b8d185434c98e86f6
ErinLlouise <erinllouise3@gmail.com>
2021-04-23 14:51:44 +1200

    styling quiz

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -9,16 +9,30 @@ function Question() {
     const [score, setScore] = useState(0)
 
     return (
-        <div className='quiz-container' >
-            <div key={question.id}>
-                <h4>{question.question}</h4>
-                <button>{question.options[0].type}. {question.options[0].option}</button>
-                <button>{question.options[1].type}. {question.options[1].option}</button>
-                <button>{question.options[2].type}. {question.options[2].option}</button>
-                <button>{question.options[3].type}. {question.options[3].option}</button>
+        <div className='container'>
+            <div key={question.id} className='grid'>
+
+                <h1 className='col'>{question.question}</h1>
+                <div className='grid'>
+                <ul className='grid'>
+                <li>
+                    <button>{question.options[0].type}. {question.options[0].option}</button>
+                    </li>
+                <li>
+                    <button>{question.options[1].type}. {question.options[1].option}</button>
+                </li>
+                <li>
+                    <button>{question.options[2].type}. {question.options[2].option}</button>
+                </li>
+
+                <li>
+                    <button>{question.options[3].type}. {question.options[3].option}</button>
+                </li>
+                </ul>
+                </div>
             </div>
             <div>
-                <Link to={`/quiz/${Number(id) + 1}`}> <button>Next</button> </Link>
+                <Link to={`/quiz/${Number(id) + 1}`}> <button className='col'>Next</button> </Link>
             </div>
         </div>
     )
